a46a3e410ce59d266cd0567fceb6b380c15cd855
Fabian Gietzen <fabian.gietzen@web.de>
2022-04-30 11:32:32 +0200

    Add interface for players with generic behavior

---
+++ ai_wars/client/behavior.py
@@ -0,0 +1,29 @@
+import abc
+
+from ..enums import EnumAction
+
+class Behavior(abc.ABC):
+	'''
+	Expresses a particular behavior for a player. This may be user input or a computer player like
+	for example a neural network.
+	'''
+
+	@abc.abstractmethod
+	def make_move(self,
+		players: dict[str, any],
+		projectiles: dict[str, any],
+		scoreboard: dict[str, int]
+	) -> set[EnumAction]:
+		'''
+		Inquires the actions the players performs given current game information.
+		See `deserializer#deserialize_game_state`
+
+		Parameters:
+			players: the position and direction of each player.
+			projectiles: the position and direction of each projectile
+			scoreboard: the score of each player
+
+		Returns:
+			a list of actions the player performs
+		'''
+		pass
