5bd38a0edaf20892e75c80a247d0074c13ebccad
jonathan <jonathan.evertz@gmail.com>
2022-05-12 16:56:59 +0200

    Added mockup model architecture for random output

--- ai_wars/models.py
+++ ai_wars/models.py
@@ -6,12 +6,15 @@ from torch import Tensor
 class DQNModel(nn.Module):
 	"""DQN Model"""
 
-	def __init__(self) -> None: # pylint: disable=useless-super-delegation
+	def __init__(self, input_dim: int, num_actions: int) -> None: # pylint: disable=useless-super-delegation
 		super().__init__()
 		self.layers = nn.Sequential(
-			nn.Identity()
+			nn.Linear(input_dim, 128),
+			nn.ReLU(inplace=True),
+			nn.Linear(128, num_actions)
 		)
 
 	def forward(self, x: Tensor) -> Tensor:
 		x = self.layers(x)
 		return x
+
