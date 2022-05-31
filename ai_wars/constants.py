fe3616a7742387229d27d568a16e99580b2a4a1a
LostOxygen <jonathan.evertz@gmail.com>
2022-05-31 09:10:48 +0200

    added height and width constant

--- ai_wars/constants.py
+++ ai_wars/constants.py
@@ -15,6 +15,8 @@ POINTS_LOST_AFTER_GETTING_HIT = 100
 POINTS_GAINED_AFTER_HITTING = 200
 
 # Client constants
+HEIGHT = 600
+WIDTH = 800
 CLIENT_BUFFER_SIZE = 10 * 1024
 
 # pygame userevents use codes from 24 to 35, so the first user event will be 24
