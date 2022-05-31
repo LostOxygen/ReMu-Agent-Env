c847d90f93f3b54dacfd8026700fdb29f3f570be
LostOxygen <jonathan.evertz@gmail.com>
2022-05-31 09:39:18 +0200

    added render to surface method

--- ai_wars/utils.py
+++ ai_wars/utils.py
@@ -7,6 +7,8 @@ import pygame
 from pygame.image import load
 from pygame.math import Vector2
 
+from .constants import (HEIGHT, WIDTH)
+
 
 def load_sprite(path: str, with_alpha=True) -> pygame.Surface:
 	loaded_sprite = load(path)
@@ -49,8 +51,46 @@ def override(func):
 	return func
 
 
-def surface_to_array(surface: pygame.Surface) -> torch.tensor:
-	"""returns the surface as a pytorch array of dimension (channels, height, width)"""
+def surface_to_tensor(surface: pygame.Surface) -> torch.tensor:
+	"""returns the surface as a pytorch tensor of dimension (channels, height, width)"""
 	img = torch.tensor(pygame.surfarray.array3d(surface))
 	img = img.permute(2, 1, 0) # swap axes to the pytorch order
 	return img
+
+
+def render_to_surface(
+    players: list[dict[str, any]],
+    projectiles: list[dict[str, any]]
+	) -> pygame.Surface:
+	"""
+	Renders the gamestate to a pygame surface
+
+	Parameters:
+		players: The players with their coordinates and directions
+		projectiles: The projectiles with their coordinates and directions
+		scoreboard: The scoreboard dictionary with the scores of the players
+		device: The device the tensor should be stored on (cpu or cuda:0)
+
+	Return:
+		pygame.Surface: the gamestate, rendered to a pygame.Surface
+	"""
+	surface = pygame.Surface((WIDTH, HEIGHT))
+	background_sprite = load_sprite("ai_wars/img/Background.png", True)
+	spaceship_sprite = load_sprite("ai_wars/img/spaceship.png", True)
+	bullet_sprite = load_sprite("ai_wars/img/bullet.png", True)
+
+	# draw the black background
+	surface.blit(background_sprite, (0, 0))
+	# draw the bullets#
+	for projectile in projectiles:
+		surface.blit(bullet_sprite, (projectile["position"].x, projectile["position"].y))
+
+	# draw the players
+	for player in players:
+		angle = player["direction"].angle_to(Vector2(0, -1))
+		rotated_surface = pygame.transform.rotozoom(spaceship_sprite, angle, 1.0)
+		rotated_surface_size = Vector2(rotated_surface.get_size())
+		blit_position = Vector2(player["position"].x, player["position"].y)-rotated_surface_size * 0.5
+		surface.blit(rotated_surface, blit_position)
+
+	return surface
