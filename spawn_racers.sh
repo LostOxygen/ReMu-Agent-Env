0b7566c583b3447af0bb91b98fe2275edea5e074
jonathan <jonathan.evertz@gmail.com>
2022-07-07 13:01:43 +0200

    added sleep

--- spawn_racers.sh
+++ spawn_racers.sh
@@ -4,6 +4,7 @@ trap "kill 0" EXIT
 for x in {0..11}; do
     gpu=$(($x % 2))
     python server.py --training_mode --verbose --addr 100.113.4.6 --port $((1330+$x)) >> server_logs$x.txt &
+    sleep 2;
     python network.py --model_type linear --name model_$x --param_search --addr 100.113.4.6 --port $((1330+$x)) --device cuda:$gpu >> training_logs$x.txt &
 done
 
